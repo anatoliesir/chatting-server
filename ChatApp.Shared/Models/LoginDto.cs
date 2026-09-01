8ac58ecda858fa422c93e7357b94eefe92c6ba9b
Răzvan <razvan.sirbu9@gmail.com>
2026-09-01 19:46:45 +0300

    Moved some files

--- ChatApp.Shared/Models/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ChatApp.Shared.Models
-{
-    public class LoginDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
